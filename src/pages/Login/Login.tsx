2cac16f47c64817f622af526c509fc9d2e75b3e6
Lucas Rodrigues <lucas.darlindo@gmail.com>
2022-05-29 00:34:14 -0300

    Tela de Login - Atualizações de Layout

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -12,24 +12,25 @@ const Login = () => {
   return (
     <SafeAreaView style={styles.login}>
       <View style={styles.header}>
-        <Text>Login</Text>
-        <Text>Acesse suas movimentações e organize suas finanças</Text>
+        <View>
+          <Text style={styles.headerText}>Login</Text>
+          <Text style={styles.headerSubText}>Acesse suas movimentações e organize suas finanças</Text>
+        </View>
+        <View>
+          <LinearGradient
+            colors={['#647AFF', '#8355E6']}
+            style={styles.headerDetail}>
+          </LinearGradient> 
+        </View>
       </View>
-      <View style={styles.headerDetail}></View>
       <View>
         <TextInput placeholder='user@example.com' style={styles.input} />
         <TextInput placeholder='******************' style={styles.input} />
         <Pressable style={styles.button} onPress={() => {console.log('Login efetuado com sucesso.')}}>
           <LinearGradient
-            // Background Linear Gradient
-            colors={['rgba(0,0,0,0.8)', 'transparent']}
-            //style={styles.background}
-          />
-          <LinearGradient
-            // Button Linear Gradient
-            colors={['#4c669f', '#3b5998', '#192f6a']}
+            colors={['#647AFF', '#8355E6']}
             style={styles.button}>
-            <Text>ENTRAR</Text>
+            <Text style={styles.buttonText}>Entrar</Text>
           </LinearGradient> 
         </Pressable>
       </View>
@@ -44,18 +45,54 @@ const styles = StyleSheet.create({
     backgroundColor: '#32364D',
   },
 
+  text: {
+    fontFamily: 'Nunito_400Regular',
+    color: 'white',
+  },
+
   header: {
     paddingBottom: 33,
     paddingTop: 60,
     paddingRight: 112,
+
+    marginLeft: 37,
+
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+
+  headerText: {
+    fontFamily: 'Nunito_700Bold',
+    fontSize: 36,
+
+    color: 'white',
+  },
+
+  headerSubText: {
+    fontSize: 8,
+
+    color: 'white',
+
+    lineHeight: 10.9,
   },
 
   headerDetail: {
-    borderRadius: 125.5,
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderBottomLeftRadius: 125.5,
+    borderColor: 'transparent',
+
+    width: 138,
+    height: 129,
+
+    alignSelf: 'flex-end',
   },
 
   input: {
     display: 'flex',
+
+    marginHorizontal: 37, 
     backgroundColor: '#434965',
     alignItems: 'center',
     justifyContent: 'center',
@@ -63,6 +100,7 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderStyle: 'solid',
     borderRadius: 15,
+    borderColor: 'transparent',
 
     paddingLeft: 31,
     paddingTop: 19,
@@ -74,13 +112,23 @@ const styles = StyleSheet.create({
 
   button: {
     alignItems: 'center',
-    backgroundColor: '#647AFF',
+
+    width: 146,
+    height: 54,
 
     borderRadius: 15,
 
     paddingVertical: 19,
+    
+    marginLeft: 26,
     marginTop: -13,
-  }
+  },
+
+  buttonText: {
+    fontFamily: 'Nunito_700Bold',
+
+    color: 'white',
+  },
 });
 
 export default Login;
